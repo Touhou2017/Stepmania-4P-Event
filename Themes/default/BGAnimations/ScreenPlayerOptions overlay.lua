34ddc1c9ed5a04481b414f3ad6e52360ce3a2d8c
Aya <142184705+Touhou2017@users.noreply.github.com>
2024-06-05 16:41:59 -0700

    Update ScreenPlayerOptions overlay.lua

--- Themes/default/BGAnimations/ScreenPlayerOptions overlay.lua
+++ Themes/default/BGAnimations/ScreenPlayerOptions overlay.lua
@@ -14,7 +14,7 @@ local function GetSpeedModHelperText(pn)
 	else
 		bpm = GAMESTATE:GetCurrentSong():GetDisplayBpms()
 		-- handle DisplayBPMs that are <= 0
-		if bpm[1] <= 0 or bpm[2] <= 0 then
+		if bpm[1] <= 0 or bpm[2] <= 0 or bpm[3] <= 0 or bpm[4] <= 0 then
 			bpm = GAMESTATE:GetCurrentSong():GetTimingData():GetActualBPM()
 		end
 	end
@@ -24,12 +24,12 @@ local function GetSpeedModHelperText(pn)
 		local musicrate = SL.Global.ActiveModifiers.MusicRate
 
 		--if a single bpm suffices
-		if bpm[1] == bpm[2] then
+		if bpm[1] == bpm[2] == bpm[3] == bpm[4] then
 			display = string.format("%.2f", speed) .. "x (" .. round(speed * bpm[1] * musicrate) .. ")"
 
 		-- if we have a range of bpms
 		else
-			display = string.format("%.2f", speed) .. "x (" .. round(speed * bpm[1] * musicrate) .. " - " .. round(speed * bpm[2] * musicrate) .. ")"
+			display = string.format("%.2f", speed) .. "x (" .. round(speed * bpm[1] * musicrate) .. " - " .. round(speed * bpm[2] * musicrate) .. " - " .. round(speed * bpm[3] * musicrate) .. " - " .. round(speed * bpm[4] * musicrate) .. ")"
 		end
 
 	-- elseif using a CMod or an MMod
@@ -58,10 +58,10 @@ local function ChangeSpeedMod(pn, direction)
 	-- elseif using a CMod or an MMod
 	elseif mods.SpeedModType == "C" or mods.SpeedModType == "M" then
 
-		if mods.SpeedMod + (5 * direction) >= 2000 then
+		if mods.SpeedMod + (5 * direction) >= 2500 then
 			mods.SpeedMod = 5
 		elseif mods.SpeedMod + (5 * direction) <= 0 then
-			mods.SpeedMod = 2000
+			mods.SpeedMod = 2500
 		else
 			mods.SpeedMod = mods.SpeedMod + (5 * direction)
 		end
@@ -88,7 +88,7 @@ local Players = GAMESTATE:GetHumanPlayers()
 
 -- SpeedModItems is a table that will contain the BitmapText Actors
 -- for the SpeedModNew OptionRow for both P1 and P2
-local SpeedModItems = {P1, P2}
+local SpeedModItems = {P1, P2, P3, P4}
 
 local t = Def.ActorFrame{
 	InitCommand=cmd(xy,_screen.cx,0),
@@ -99,17 +99,25 @@ local t = Def.ActorFrame{
 		local ScreenOptions = SCREENMAN:GetTopScreen()
 
 		-- reset for ScreenEditOptions
-		SpeedModItems = {P1 = nil, P2 = nil}
+		SpeedModItems = {P1 = nil, P2 = nil, P3 = nil, P4 = nil}
 
-		-- The bitmaptext actors for P1 and P2 speedmod are both named "Item"
+		-- The bitmaptext actors for P1 and P2 and P3 and P4 speedmod are both named "Item"
 		SpeedModItems.P1 = ScreenOptions:GetOptionRow(FindSpeedModOptionRowIndex(ScreenOptions)):GetChild(""):GetChild("Item")[1]
 		SpeedModItems.P2 = ScreenOptions:GetOptionRow(FindSpeedModOptionRowIndex(ScreenOptions)):GetChild(""):GetChild("Item")[2]
+		SpeedModItems.P3 = ScreenOptions:GetOptionRow(FindSpeedModOptionRowIndex(ScreenOptions)):GetChild(""):GetChild("Item")[3]
+		SpeedModItems.P4 = ScreenOptions:GetOptionRow(FindSpeedModOptionRowIndex(ScreenOptions)):GetChild(""):GetChild("Item")[4]
 
 		if SpeedModItems.P1 and GAMESTATE:IsPlayerEnabled(PLAYER_1) then
 			self:playcommand("SetP1")
 		end
 		if SpeedModItems.P2 and GAMESTATE:IsPlayerEnabled(PLAYER_2) then
 			self:playcommand("SetP2")
+		end
+		if SpeedModItems.P3 and GAMESTATE:IsPlayerEnabled(PLAYER_3) then
+			self:playcommand("SetP3")
+		end
+		if SpeedModItems.P4 and GAMESTATE:IsPlayerEnabled(PLAYER_4) then
+			self:playcommand("SetP4")
 		end
 	end
 }
@@ -136,7 +144,7 @@ for player in ivalues(Players) do
 					bpm = GetCourseModeBPMs()
 				else
 					bpm = song:GetDisplayBpms()
-					if bpm[1] <= 0 or bpm[2] <= 0 then
+					if bpm[1] <= 0 or bpm[2] <= 0 or bpm[3] <= 0 or bpm[4] <= 0 then
 						bpm = song:GetTimingData():GetActualBPM()
 					end
 				end
@@ -196,9 +204,13 @@ for player in ivalues(Players) do
 		InitCommand=function(self)
 			self:diffuse(PlayerColor(player))
 			self:zoom(0.5)
-			if player == PLAYER_1 then
+				if player == PLAYER_1 then
 				self:x(-100)
 			elseif player == PLAYER_2 then
+				self:x(-17)
+			elseif player == PLAYER_3 then
+				self:x(66)
+			elseif player == PLAYER_4 then
 				self:x(150)
 			end
 			self:y(48)
@@ -222,16 +234,24 @@ for player in ivalues(Players) do
 			self:zoom(0.9)
 
 			if IsUsingWideScreen() then
-				if player == PLAYER_1 then
+					if player == PLAYER_1 then
 					self:x(-100)
 				elseif player == PLAYER_2 then
+					self:x(-17)
+				elseif player == PLAYER_3 then
+					self:x(66)
+				elseif player == PLAYER_4 then
 					self:x(150)
 				end
 				self:y(26)
 			else
-				if player == PLAYER_1 then
+					if player == PLAYER_1 then
 					self:x(-10)
 				elseif player == PLAYER_2 then
+					self:x(73)
+				elseif player == PLAYER_3 then
+					self:x(156)
+				elseif player == PLAYER_4 then
 					self:x(240)
 				end
 				self:y(50)
@@ -267,7 +287,7 @@ for player in ivalues(Players) do
 			if song then
 				bpms = song:GetDisplayBpms()
 				-- if either display BPM is negative or 0, use the actual BPMs instead...
-				if bpms[1] <= 0 or bpms[2] <= 0 then
+				if bpms[1] <= 0 or bpms[2] <= 0 or bpms[3] <= 0 or bpms[4] <= 0 then
 					bpms = song:GetTimingData():GetActualBPM()
 				end
 			else
@@ -279,12 +299,14 @@ for player in ivalues(Players) do
 			-- truncate possible decimal places
 			bpms[1] = ("%0.0f"):format(bpms[1])
 			bpms[2] = ("%0.0f"):format(bpms[2])
+			bpms[3] = ("%0.0f"):format(bpms[3])
+			bpms[4] = ("%0.0f"):format(bpms[4])
 
 			-- settext on "Speed Mod" OptionRow title
-			if bpms[1] == bpms[2] then
+			if bpms[1] == bpms[2] == bpms[3] == bpms[4] then
 				SpeedModTitle:settext( THEME:GetString("OptionTitles", "SpeedMod") .. " (" .. bpms[1] * musicrate .. ")" )
 			else
-				SpeedModTitle:settext( THEME:GetString("OptionTitles", "SpeedMod") .. " (" .. bpms[1] * musicrate ..  " - " .. bpms[2] * musicrate  .. ")" )
+				SpeedModTitle:settext( THEME:GetString("OptionTitles", "SpeedMod") .. " (" .. bpms[1] * musicrate ..  " - " .. bpms[2] * musicrate  .. " - " .. bpms[3] * musicrate  .. " - " .. bpms[4] * musicrate  .. ")"  )
 			end
 		end,
 		MusicRateChangedMessageCommand=cmd(playcommand,"Set")
