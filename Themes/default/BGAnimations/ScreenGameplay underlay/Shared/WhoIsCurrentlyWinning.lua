04213057469a73fd581ff131206de7ca15da03d3
Aya <142184705+Touhou2017@users.noreply.github.com>
2024-06-05 16:26:39 -0700

    Update WhoIsCurrentlyWinning.lua

--- Themes/default/BGAnimations/ScreenGameplay underlay/Shared/WhoIsCurrentlyWinning.lua
+++ Themes/default/BGAnimations/ScreenGameplay underlay/Shared/WhoIsCurrentlyWinning.lua
@@ -3,19 +3,89 @@ return Def.Actor{
 	WinningCommand=function(self)
 		local dpP1 = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_1):GetPercentDancePoints()
 		local dpP2 = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_2):GetPercentDancePoints()
+		local dpP3 = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_3):GetPercentDancePoints()
+		local dpP4 = STATSMAN:GetCurStageStats():GetPlayerStageStats(PLAYER_4):GetPercentDancePoints()
 
 		local p1_score = self:GetParent():GetChild("P1Score")
 		local p2_score = self:GetParent():GetChild("P2Score")
+		local p3_score = self:GetParent():GetChild("P3Score")
+		local p4_score = self:GetParent():GetChild("P4Score")
 
-		if dpP1 == dpP2 then
-			p1_score:diffusealpha(1)
-			p2_score:diffusealpha(1)
-		elseif dpP1 > dpP2 then
-			p1_score:diffusealpha(1)
-			p2_score:diffusealpha(0.65)
-		elseif dpP2 > dpP1 then
-			p1_score:diffusealpha(0.65)
-			p2_score:diffusealpha(1)
+		if dpP1 == dpP2 == dpP3 == dpP4 then --P1=P2=P3=P4 All 4/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(1.00)
+		elseif dpP2 == dpP3 == dpP4 > dpP1 then -- P2=P3=P4>P1 3/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(1.00)
+		elseif dpP1 == dpP3 == dpP4 > dpP2 then -- P1=P3=P4>P2 3/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(1.00)
+		elseif dpP1 == dpP2 == dpP4 > dpP3 then -- P1=P2=P4>P3 3/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(1.00)
+		elseif dpP1 == dpP2 == dpP3 > dpP4 then -- P1=P2=P3>P4 3/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(0.50)
+		elseif dpP1 == dpP2 > dpP3 and dpP4 then -- P1=P2>P3&P4 2/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(0.50)
+		elseif dpP2 == dpP3 > dpP1 and dpP4 then -- P2=P3>P1&P4 2/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(0.50)
+		elseif dpP3 == dpP4 > dpP1 and dpP2 then -- P3=P4>P1&P2 2/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(1.00)
+		elseif dpP4 == dpP1 > dpP2 and dpP3 then -- P4=P1>P2&P3 2/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(1.00)
+		elseif dpP1 == dpP3 > dpP2 and dpP4 then -- P1=P3>P2&P4 2/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(0.50)
+		elseif dpP2 == dpP4 > dpP1 and dpP3 then -- P2=P4>P1&P3 2/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(1.00)
+		elseif dpP1 > dpP2 and dpP3 and dpP4 then -- P1>P2&P3&P4 1/4
+			p1_score:diffusealpha(1.00)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(0.50)
+		elseif dpP2 > dpP1 and dpP3 and dpP4 then -- P2>P1&P3&P4 1/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(1.00)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(0.50)
+		elseif dpP3 > dpP1 and dpP2 and dpP4 then -- P3>P1&P2&P4 1/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(1.00)
+			p4_score:diffusealpha(0.50)
+		elseif dpP4 > dpP1 and dpP2 and dpP3 then -- P4>P1&P2&P3 1/4
+			p1_score:diffusealpha(0.50)
+			p2_score:diffusealpha(0.50)
+			p3_score:diffusealpha(0.50)
+			p4_score:diffusealpha(1.00)
 		end
 	end
 }
